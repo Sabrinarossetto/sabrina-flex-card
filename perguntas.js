cb49a3190aead125ace3aa1ca0fa2f1fdf77f430
Sabrinarossetto <sabrinarossetto202007@gmail.com>
2024-11-28 08:34:04 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'como diz animal em inglês?',
+    'animal em inglês é énimal'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um animal carnívoro?',
+    'Um animal carnívoro é aquele que se alimenta principalmente de carne, como leões, lobos e crocodílos'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
